7afa991d173d230dbca8360d47b51e0d781344ec
Matias Joel Giannini <111232232+JoelGiannini@users.noreply.github.com>
2024-07-22 22:44:51 -0300

    Add files via upload

--- Trivia.py
+++ Trivia.py
@@ -1,6 +1,13 @@
 import random
 import os
 import json
+
+#Color de fuentes
+rojo=chr(27)+"[31;1m"
+amarillo=chr(27)+"[33;1m"
+verde=chr(27)+"[32;1m"
+blanco=chr(27)+"[33;97m"
+
 scenery = \
 '''   
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -17,25 +24,24 @@ scenery = \
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-Desarrollado por Grupo Saturno.
-'''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'''
 
 def menu():
     os.system('clear')
-    print(f'{scenery}')
+    print(amarillo + scenery)
+    print (blanco +"Desarrollado por Grupo Saturno.\n\n")
     print('Te damos la bienvenia a la triva de paises de latinoamerica')
     letter = input('presione una tecla  para continuar...').lower()
     os.system('clear')
     print('Reglamento:\n\n')
     print('Tendras que adivinar las capitales de todos los paises de latino america.\nCada acierrto suma 2 puntos, por contrario, cada falla resta 2 puntos y si utilizamos una pista se resta un punto\n\n')
-    pause= input('presione una tecla  para continuar...').lower()
+    pause= input(verde +'Presione una tecla  para continuar...').lower()
 
 def carga_data():
     with open("paises.json") as file:
         data=json.load(file)
         return data
-
+    
 def paises(data):
         for i, j  in zip(data.keys(), data.values()):
             if i == "pais":
@@ -56,6 +62,18 @@ def pistas(data):
     pista=random.choice(pista)
     return pista
 
+def normalizar(s):
+    reemplazar = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+    for a, b in reemplazar:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+    return s
+
 def start():
     menu()
     lista=carga_data()
@@ -67,59 +85,80 @@ def start():
     for i in lista:
         pais=i["pais"]
         capital=i["capital"]
-        respuesta = input(f"¿Cuál es la capital de {pais}?\n")
+        respuesta = input(blanco + f"¿Cuál es la capital de {pais}?\n")
+        respuesta=normalizar(respuesta)
         if respuesta.lower() == capital.lower():
             os.system("clear")
-            print("¡Correcto!")
-            puntos += 2
+            print(verde + "¡Correcto!")
+            print("")
+            puntos += 5
             aciertos+=1
-
         else:
             os.system("clear")
-            print("Incorrecto.")
+            print(rojo + "Incorrecto.")
+            print("")
             errores += 1
             puntos -= 2
-            opcion_pista = input("¿Quieres una pista? (sí/no):\n").strip().lower()
+            opcion_pista = input(blanco + "¿Quieres una pista? (sí/no):\n").strip().lower()
+            opcion_pista=normalizar(opcion_pista)
             while (opcion_pista != "si" and opcion_pista != "no"):
                 opcion_pista = input("Ingresaste una opcion incorrecta por favor ingresar (si/no):\n").strip().lower()
+                opcion_pista=normalizar(opcion_pista)
             if opcion_pista == 'si':
                 os.system("clear")
                 puntos -= 1
                 total_pistas += 1
                 pista = pistas(i)
-                respuesta_ayuda = input(f"Pista:\n{pista}.\n\nUltimo intento.\n\n¿Cuál es la capital de {pais}?\n")
+                print ("Pista:")
+                print (amarillo + f"{pista}")
+                print (blanco + "")
+                respuesta_ayuda = input(f"Ultimo intento.\n\n¿Cuál es la capital de {pais}?\n")
+                respuesta_ayuda=normalizar(respuesta_ayuda)
                 if respuesta_ayuda.lower() == capital.lower():
                     os.system("clear")
-                    print("¡Correcto!\n")
-                    puntos += 2
+                    print(verde + "¡Correcto!")
+                    print("")
+                    puntos += 5
                     aciertos += 1
                 else:
                     os.system("clear")
-                    print(f"Incorrecto nuevamente. La capital de {pais} es {capital}.\n")
+                    print(rojo + "Incorrecto nuevamente.")
+                    print ("")
+                    print(blanco + f"La capital de {pais} es " + amarillo +  f"{capital}.")
+                    print("")
                     errores += 1
                     puntos -= 2
             elif opcion_pista == 'no':
                  os.system("clear")
                  print (f"Ultimo intento para adivinar la capital.\n")
                  respuesta = input(f"¿Cuál es la capital de {pais}?\n")
+                 respuesta=normalizar(respuesta)
                  if respuesta.lower() == capital.lower():
                     os.system("clear")
-                    print("¡Correcto!")
-                    puntos += 2
+                    print(verde + "¡Correcto!")
+                    print("")
+                    puntos += 5
                     aciertos+=1
                  else:
                     os.system("clear")
-                    print("Incorrecto.")
+                    print(rojo + "Incorrecto.")
+                    print ("")
                     errores += 1
                     puntos -= 2
-                    print(f"La capital de {pais} es {capital}.\n")
-            else:
-                os.system("clear")
-                print(f"La capital de {pais} es {capital}.\n")
-        print()
+                    print(blanco + f"La capital de {pais} es " + amarillo +  f"{capital}.")
+                    print("")
     os.system("clear")
-    print (f"Felicidades has terminado  la trivia.\n\nSu puntuacion total es de:\n\nPuntos: {puntos} sobre 22 puntos\nCorrectas:{aciertos}\nErroneas:{errores}\nCantidad de pistas utilizadas:{total_pistas}")
-
+    print (f"Felicidades has terminado  la trivia.\n\nSu puntuacion total es de:\n\nPuntos: {puntos} sobre 55 puntos\nCorrectas:{aciertos}\nErroneas:{errores}\nCantidad de pistas utilizadas:{total_pistas}")
+    salida=input(f"\n\n¿Queres volver a jugar? (si/no)\n")
+    salida=normalizar(salida)
+    while (salida != "si" and salida != "no"):
+        salida = input("Ingresaste una opcion incorrecta por favor ingresar (si/no):\n").strip().lower()
+        salida=normalizar(salida)
+    if salida == 'si':
+        start()
+    else:
+        os.system("clear")
+        exit(1)
 
 if __name__ == '__main__':
 	
